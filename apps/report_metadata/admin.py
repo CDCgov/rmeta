b47ef584f952778c7078572fb436ed0b5fe09879
Alan Viars <aviars@videntity.com>
2024-04-22 11:17:34 -0400

    add last login to User admin

--- apps/report_metadata/admin.py
+++ apps/report_metadata/admin.py
@@ -6,6 +6,14 @@ from .models import (HealthDataType, DataTransportType, PatientIDType,
                      AnonyomizedDataNeed,  ProgramAreaType,IntermediarySoftware,
                      IntermediarySystem, PersonHashType, InergrationEngineSoftware, 
                      InergrationEngineSystem)
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login') # Added last_login
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 
 
