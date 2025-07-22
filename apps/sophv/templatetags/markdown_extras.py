9af1741a904792ed4df83e78a1b9062126bb52f8
Alan Viars <aviars@videntity.com>
2025-07-22 10:57:14 -0400

    add missing temaplatetag for markdown rendering

---
+++ apps/sophv/templatetags/markdown_extras.py
@@ -0,0 +1,11 @@
+# your_app_name/templatetags/markdown_extras.py
+from django import template
+from django.template.defaultfilters import stringfilter
+import markdown as md
+
+register = template.Library()
+
+@register.filter()
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
